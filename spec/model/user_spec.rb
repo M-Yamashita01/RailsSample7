b5b7c244b467c2b18d2d280ab354a12aada01b41
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2024-08-16 00:06:52 +0900

    update spec

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -5,238 +5,59 @@ RSpec.describe User, type: :model do
   let(:name2) { 'John Doe2' }
   let(:email) { 'example@example.com' }
 
-  describe '#create' do
-    it 'should create user' do
-      puts '---create---'
-      User.create(name:, email:)
-    end
-
-    context 'when user.save' do
-      before do
-        User.create(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create → save---'
-        user = User.last
-        user.name = name2
-        user.save
-      end
-    end
-
-    context 'when user.save!' do
-      before do
-        User.create(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create → save!---'
-        user = User.last
-        user.name = name2
-        user.save!
-      end
-    end
-
-    context 'when user.update' do
-      before do
-        User.create(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create → update---'
-        user = User.last
-        user.update(name: name2)
-      end
-    end
-
-    context 'when user.update!' do
-      before do
-        User.create(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create → update!---'
-        user = User.last
-        user.update!(name: name2)
-      end
-    end
-  end
-
-  describe '#create!' do
-    it 'should create user' do
+  describe 'create!' do
+    it 'create user' do
       puts '---create!---'
       User.create!(name:, email:)
     end
-
-    context 'when user.save' do
-      before do
-        User.create!(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create! → save---'
-        user = User.last
-        user.name = name2
-        user.save
-      end
-    end
-
-    context 'when user.save!' do
-      before do
-        User.create!(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create! → save!---'
-        user = User.last
-        user.name = name2
-        user.save!
-      end
-    end
-
-    context 'when user.update' do
-      before do
-        User.create!(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create! → update---'
-        user = User.last
-        user.update(name: name2)
-      end
-    end
-
-    context 'when user.update!' do
-      before do
-        User.create!(name:, email:)
-      end
-
-      it 'should update user' do
-        puts '---create! → update!---'
-        user = User.last
-        user.update!(name: name2)
-      end
-    end
   end
 
-  describe '#save' do
-    it 'should save user' do
-      puts '---save---'
-      user = User.new(name:, email:)
-      user.save
-    end
-
-    context 'when user.save' do
-      before do
-        user = User.new(name:, email:)
-        user.save
-      end
-
-      it 'should update user' do
-        puts '---save → save---'
-        user = User.last
-        user.name = name2
-        user.save
-      end
-    end
-
-    context 'when user.save!' do
-      before do
-        user = User.new(name:, email:)
-        user.save
-      end
-
-      it 'should update user' do
-        puts '---save → save!---'
-        user = User.last
-        user.name = name2
-        user.save!
-      end
-    end
-
-    context 'when user.update' do
-      before do
-        user = User.new(name:, email:)
-        user.save
-      end
-      it 'should update user' do
-        puts '---save → update---'
-        user = User.last
-        user.update(name: name2)
-      end
-    end
-
-    context 'when user.update!' do
-      before do
-        user = User.new(name:, email:)
-        user.save
-      end
-
-      it 'should update user' do
-        puts '---save → update!---'
-        user = User.last
-        user.update!(name: name2)
-      end
-    end
-  end
-
-  describe '#save!' do
-    it 'should save user' do
+  describe 'save!' do
+    it 'save user' do
       puts '---save!---'
       user = User.new(name:, email:)
       user.save!
     end
 
-    context 'when user.save' do
-      before do
-        user = User.new(name:, email:)
-        user.save!
-      end
-
-      it 'should update user' do
-        puts '---save! → save---'
-        user = User.last
-        user.name = name2
-        user.save
-      end
-    end
-
-    context 'when user.save!' do
-      before do
-        user = User.new(name:, email:)
-        user.save!
-      end
-
-      it 'should update user' do
-        puts '---save! → save!---'
-        user = User.last
+    context 'create! → save!' do
+      it 'save user' do
+        puts '---create!---'
+        user = User.create!(name:, email:)
+        puts '---save!---'
+        # user = User.last
         user.name = name2
         user.save!
       end
     end
 
-    context 'when user.update' do
-      before do
+    context 'save! → save!' do
+      it 'update user' do
+        puts '---save!---'
         user = User.new(name:, email:)
         user.save!
+        puts '---save!---'
+        user.name = name2
+        user.save!
       end
+    end
+  end
 
-      it 'should update user' do
-        puts '---save! → update---'
-        user = User.last
-        user.update(name: name2)
+  describe 'update!' do
+    context 'create! → update!' do
+      it 'update user' do
+        puts '---create!---'
+        user = User.create!(name:, email:)
+        puts '---update!---'
+        user.update!(name: name2)
       end
     end
 
-    context 'when user.update!' do
-      before do
+    context 'save! → update!' do
+      it 'update user' do
+        puts '---save!---'
         user = User.new(name:, email:)
         user.save!
-      end
-
-      it 'should update user' do
-        puts '---save! → update!---'
-        user = User.last
+        puts '---update!---'
         user.update!(name: name2)
       end
     end
